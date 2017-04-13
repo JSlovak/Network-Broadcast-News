f1dfc0d3313b57afbe75e9a725f746335e1c9412
JSlovak <jasmine.slovak@gmail.com>
2017-04-12 16:06:27 -1000

    Additional feature Admin Broadcast working.

--- js/client.js
+++ js/client.js
@@ -1,2 +1,20 @@
 /*jshint esversion: 6 */
 
+const net = require('net');
+
+const client = net.connect({port:3000}, ()=>{
+  console.log("You've entered chatroom at PORT: 3000.");
+  client.write("Someone new has entered the chatroom.");
+
+  let username = null;
+
+  process.stdin.on('data', (chunk) =>{
+    // console.log(chunk.toString());
+    client.write(chunk);
+  });
+
+  client.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+});
--- js/server.js
+++ js/server.js
@@ -1,28 +1,32 @@
 /*jshint esversion: 6 */
 
 const net = require('net');
+let users = [];
+
 
 // This "listens" for new Server Connections. The listeners/commands are for what happens to with ea new established client connection.
 const server = net.createServer((connection)=> {
   console.log("New Server Connection Established");
+  let username = null;
 
-  //I want the server to write to the client on their connection to write greeting, and ask client to designate a "username"
-  // Store that username to variable
-  // Use that variable before every input with write function.
+  // Add new user to user array
+  users.push(connection);
 
-    connection.on('data', (data) => {
-      console.log(data.string());
-      connection.write(data.string());
+  // Share message with user, if user is not myself
+  connection.on('data', (data)=> {
+    for(let i= 0; i<users.length; i++){
+      if (users[i] !== connection){
+        users[i].write(data.toString());
+      }
+    }
+  });
 
-    });
-
-    connection.on('end' (person)=>{
-      connection.write(`${person} left has left the room`);
-
-    });
+  process.stdin.on('data', (chunk) =>{
+    connection.write(`[ADMIN]:${chunk}`);
+  });
 });
 
 // This initiates the Server
 server.listen(3000,()=>{
-  console.log ("Your chat session has opened. You're currently using port 3000");
+  console.log ("You've opened a chat session. You're currently the Administrator. You're currently using port 3000");
 });
