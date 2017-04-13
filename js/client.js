5ac96499124a41c8a310fc15cca5441fb87e8135
JSlovak <jasmine.slovak@gmail.com>
2017-04-13 11:47:07 -1000

    Code & note clean-up.

--- js/client.js
+++ js/client.js
@@ -2,6 +2,8 @@
 
 const net = require('net');
 
+
+
 const client = net.connect({port:3000, host:"10.0.1.3"}, ()=>{
   // client.write("Someone new has entered the chatroom.");
 
--- js/server.js
+++ js/server.js
@@ -1,43 +1,81 @@
 /*jshint esversion: 6 */
 
 const net = require('net');
+
+// Keeps track of users
 let users = [];
 
+// Defines the port
+let port = 3000;
 
-// This "listens" for new Server Connections. The listeners/commands are for what happens to with ea new established client connection.
+// Initiates a server
 const server = net.createServer((connection)=> {
-  console.log("New Server Connection Established");
 
+  // Let's admin know when a new user has connected
+  console.log("A new-user has signed into the chatroom");
+
+  //Prompts the user for a username
   connection.write("Welcome to chatroom at PORT 3000, please enter a username.");
 
   // Add new user to user array
   users.push(connection);
-  let username = null;
+  connection.username = null;
 
   // Share message with user, if user is not myself
   connection.on('data', (data)=> {
+
+    // traverse array of users
     for(let i= 0; i<users.length; i++){
-      if (username === null){
-        username = data.toString();
-        console.log(username + " has set their username.");
-        users[i].write(`${username} joined the chatroom.`);
-      }
-      else {
-        console.log(data.toString());
+
+      // Check to see if username has been set//
+      if (connection.username === null){
+
+        // If not, set username
+        connection.username = data.toString();
+
+        // Notify Admin that username has been set
+        process.stdout.write( "This new User has set their username to " + connection.username);
+
+        // Notify room that user has joined
         if (users[i] !== connection){
-          users[i].write(username + ":" + data.toString());
-          console.log(username + data.toString());
+          users[i].write(connection.username.trim() + " joined the chatroom.");
+        } else {
+
+        // Feedback to user that their username has been set
+          connection.write("You're username has been set to " + connection.username);
+        }
+      }
+
+      else {
+        // format Buffer to string data-type
+        let message = data.toString();
+
+        // UX, notify user if only one in chatroom
+        if (users.length <= 1){
+          connection.write("Currently, there is no one else in the chatroom.");
+          process.stdout.write(connection.username.trim() + " : " + message);
+        }
+
+
+        // Broadcast to users that are not this one
+        if (users[i] !== connection){
+          users[i].write(connection.username.trim() + " : " + message);
+
+          // Broadcast to Admin
+          process.stdout.write(connection.username.trim() + " : " + message);
         }
       }
     }
   });
-
+// Allow for Admin to broadcast with denotation '[ADMIN]:'
   process.stdin.on('data', (chunk) =>{
     connection.write(`[ADMIN]:${chunk}`);
   });
 });
 
 // This initiates the Server
-server.listen(3000,()=> {
-  console.log ("You've opened a chat session. You're currently the Administrator. You're currently using port 3000");
+server.listen(port,()=> {
+  console.log (`You've opened a chat session. You're currently the Administrator. You're currently using port ${port}`);
 });
+
+
